26230523606cd91717012bd7a529b9ac3aa7f1ce
Renatog2 <r.g.moraes@hotmail.com>
2023-09-23 14:54:00 -0300

    Functions in Header Buttons Completed

--- script2.js
+++ script2.js
@@ -1,15 +1,45 @@
 function fontSize(type) {
-  var ids = ["#HeaderButton"]
-  ids.forEach((id) => {
-    var elemento = document.querySelector(id)
-    var size = window
+  var elementos = document.querySelectorAll("*")
+  elementos.forEach(function (elemento) {
+    var tamanhoDaFonte = window
       .getComputedStyle(elemento, null)
       .getPropertyValue("font-size")
-    size = parseFloat(size)
+    tamanhoDaFonte = parseFloat(tamanhoDaFonte)
     if (type == "Increase") {
-      elemento.style.fontSize = size + 2 + "px"
+      if (tamanhoDaFonte <= 20) {
+        elemento.style.fontSize = tamanhoDaFonte + 2 + "px"
+      }
+    } else if (type == "Decrease") {
+      if (tamanhoDaFonte >= 12) {
+        elemento.style.fontSize = tamanhoDaFonte - 2 + "px"
+      }
     } else {
-      elemento.style.fontSize = size - 2 + "px"
+      elemento.style.fontSize = 16 + "px"
     }
   })
+}
+
+function fontStrong(botao) {
+  var elementos = document.querySelectorAll("*")
+  elementos.forEach(function (elemento) {
+    var intensidadeDaFonte = window
+      .getComputedStyle(elemento, null)
+      .getPropertyValue("font-weight")
+    intensidadeDaFonte = parseFloat(intensidadeDaFonte)
+
+    if (intensidadeDaFonte == 400) {
+      elemento.style.fontWeight = 700
+    } else {
+      elemento.style.fontWeight = 400
+    }
+  })
+}
+
+function fontChange() {
+  const html = document.documentElement
+  if (html.classList.contains("contrast")) {
+    html.classList.remove("contrast")
+  } else {
+    html.classList.add("contrast")
+  }
 }
